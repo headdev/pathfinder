530a3c7a7b346c46a90efd40e9ca15b1c49340c8
CFW HeadDev <31333238+headdev@users.noreply.github.com>
2024-06-24 00:49:02 -0500

    coomit
    ahora calculamos todo

--- src/constants.ts
+++ src/constants.ts
@@ -5,11 +5,16 @@ enum DEX {
 const MIN_TVL = 50_000;
 const DEFAULT_TIMEOUT = 5000; //ms
 const DEFAULT_TOKEN_NUMBER = 5;
+const SLIPPAGE = 0.005; // 0.5% slippage
+const LENDING_FEE = 0.005; // 0.5% lending fee
+
 
 
 export {
     DEX,
     MIN_TVL,
     DEFAULT_TIMEOUT,
-    DEFAULT_TOKEN_NUMBER
+    DEFAULT_TOKEN_NUMBER,
+    SLIPPAGE,
+    LENDING_FEE 
 }
